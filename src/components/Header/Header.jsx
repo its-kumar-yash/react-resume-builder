84d870766bb46cf8b303a0110b927b9b4f95c6d2
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-25 21:03:46 +0530

    added styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import styles from "./Header.module.css";
-import resumeSvg from "../../assets/resume.svg";
+import resumeBanner from "../../assets/resume1.svg";
 
 const Header = () => {
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <p className={styles.heading}>
-        A <span>Resume</span> that stands out!
-      </p>
-      <p className={styles.heading}>
-        Make your own Resume.<span>It's free</span>
-      </p>
+          If You Want To Get Gaining, Get A Resume
+        </p>
+        <p className={styles.subHeading}>
+          ResumeXplorer is a tool that often constitutes an automated process in
+          which you follow a template and input your information. Ability to
+          build, print, and download your resume for free in minutes.
+        </p>
+        <button className={styles.btn}>Build Resume</button>
       </div>
       <div className={styles.right}>
-        <img src={resumeSvg} alt="Resume" />
+        <img src={resumeBanner} alt="Resume" />
       </div>
     </div>
   );
